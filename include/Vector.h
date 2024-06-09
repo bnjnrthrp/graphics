f8c37c9d946289632a3ffedcc8aaf58164ace3ca
Benji Northrop <benji.northrop@gmail.com>
2024-06-09 05:47:58 -0700

    func: init vector.c / .h and implement functions

---
+++ include/Vector.h
@@ -0,0 +1,16 @@
+#ifndef VECTOR_H
+
+#define VECTOR_H
+
+#include "Point.h"
+typedef Point Vector;
+
+void vector_set(Vector *v, double x, double y, double z);
+void vector_print(Vector *v, FILE *fp);
+void vector_copy(Vector *dest, Vector *src);
+double vector_length(Vector *v);
+void vector_normalize(Vector *v);
+double vector_dot(Vector *a, Vector *b);
+void vector_cross(Vector *a, Vector *b, Vector *c);
+
+#endif // VECTOR_H
