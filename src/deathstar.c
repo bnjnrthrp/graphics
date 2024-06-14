f29507be4878f250b87383b5052606325ff7565b
Benji Northrop <benji.northrop@gmail.com>
2024-06-13 19:48:12 -0700

    project: Spinning sphere complete

--- src/deathstar.c
+++ src/deathstar.c
@@ -16,17 +16,19 @@ int main(int argc, char *argv[])
     const int rows = 250;
     const int cols = 250;
     const int Resolution = 15;
-    const int nFrames = 10;
+    const int nFrames = 25;
     View3D view;
-    Color White;
+    Color White, Black;
     Point unitSphere[Resolution * Resolution];
     Point tpt[Resolution * Resolution];
+    Point ptt[Resolution * Resolution];
     int i, j, t;
     Matrix vtm;
     double center;
     char filename[256];
 
     color_set(&White, 1, 1, 1);
+    color_set(&Black, 0, 0, 0);
 
     // Set up the unit sphere
     for (i = 0; i < Resolution; i++)
@@ -43,44 +45,31 @@ int main(int argc, char *argv[])
                       1);
         }
     }
-
-    // Put points into a temporary polygon for printing
-    for (i = 0; i < Resolution * Resolution; i++)
-    {
-        point_copy(&(tpt[i]), &(unitSphere[i]));
-        printf("Copied: %d:", i);
-        point_print(&(tpt[i]), stdout);
-    }
-
-    // // grab command line argument to determine viewpoint
-    // // and set up the view structure
-    // if (argc > 1)
+    // matrix_rotateX(&vtm, cos(M_PI / 4.0), sin(M_PI / 4.0));
+    // // Put points into a temporary polygon for printing
+    // for (i = 0; i < Resolution * Resolution; i++)
     // {
-    //     float alpha = atof(argv[1]);
-    //     if (alpha < 0.0 || alpha > 1.0)
-    //         alpha = 0.0;
+    //     point_copy(&(ptt[i]), &(unitSphere[i]));
+    //     matrix_xformPoint(&vtm, &(ptt[i]), &(tpt[i]));
+    // }
 
-    //     point_set(&(view.vrp), 3 * alpha, 2 * alpha, -2 * alpha - (1.0 - alpha) * 3, 1.0);
-    // }
-    // else
-    // {
-    //     point_set(&(view.vrp), 3, 2, -2, 1.0);
-    // }
+    vector_set(&(view.vup), 0, 1, 0);
+    view.d = 1.0; // focal length
+    view.du = 2.0;
+    view.dv = 2.0;
+    view.f = 0; // front clip plane
+    view.b = 4; // back clip plane
+    view.screenx = cols;
+    view.screeny = rows;
+
     src = image_create(rows, cols);
-    for (i = 0; i < 50; i++)
+    for (t = 0; t < nFrames; t++)
     {
-        float alpha = (float)i / 50;
-        point_set(&(view.vrp), 3 * alpha, 2 * alpha, -2 * alpha - (1.0 - alpha) * 3, 1.0);
+        image_fillc(src, Black);
+        // rotate the VRP to its correct position
+        float alpha = (float)(t * 4.0) / 360.0;
+        point_set(&(view.vrp), cos(2.0 * M_PI * alpha), 2, sin(2.0 * M_PI * alpha), 1.0);
         vector_set(&(view.vpn), -view.vrp.val[0], -view.vrp.val[1], -view.vrp.val[2]);
-
-        vector_set(&(view.vup), 0, 1, 0);
-        view.d = 1; // focal length
-        view.du = 2;
-        view.dv = view.du * (float)rows / cols;
-        view.f = 0; // front clip plane
-        view.b = 4; // back clip plane
-        view.screenx = cols;
-        view.screeny = rows;
 
         matrix_setView3D(&vtm, &view);
         // Create the image
@@ -88,18 +77,15 @@ int main(int argc, char *argv[])
         for (int i = 0; i < Resolution * Resolution; i++)
         {
             matrix_xformPoint(&vtm, &(unitSphere[i]), &(tpt[i]));
-            printf("Normalizing point number %d\n", i);
-            point_print(&(tpt[i]), stdout);
             point_normalize(&(tpt[i]));
 
             point_draw(&(tpt[i]), src, White);
         }
 
-        printf("That's no moon, that's a space station\n");
-        sprintf(filename, "deathstar-%04d.ppm", i);
+        sprintf(filename, "sphere-%02d.ppm", t);
         image_write(src, filename);
     }
-
     image_free(src);
+
     return (0);
 }
