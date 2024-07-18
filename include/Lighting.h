1f149d0af565ce2644344abf09e772d463d9f60c
Benji Northrop <benji.northrop@gmail.com>
2024-07-18 13:19:32 -0700

    Archive all images and files for project

--- include/Lighting.h
+++ include/Lighting.h
@@ -2,8 +2,30 @@
 
 #define LIGHTING_H
 
+#define MAX_LIGHTS 64
+
+typedef enum LightType
+{
+    LightNone,
+    LightAmbient,
+    LightDirect,
+    LightPoint,
+    LightSpot,
+} LightType;
+
+typedef struct Light
+{
+    LightType type;
+    Color color;
+    Vector direction;
+    Point position;
+    float cutoff;    // Stores the cosine of the cutoff angle of a spotlight
+    float sharpness; // Coefficient of the falloff function (power of cosine)
+} Light;
+
 typedef struct Lighting
 {
     int nLights;
+    Light light[MAX_LIGHTS];
 } Lighting;
 #endif // LIGHTING_H
